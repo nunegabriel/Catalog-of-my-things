f5b73f490734be2fafad4a4648c6782bfdd3f002
nunegabriel <nune.gabriel.gn@gmail.com>
2023-02-01 09:17:32 +0000

    ADD MUSIC LIBRARY

---
+++ src/music_album.rb
@@ -0,0 +1,18 @@
+require 'item'
+
+class MusicAlbum < Item
+    atter_reader :on_spotify
+
+    def initialize(publish_date, on_spotify, archived )
+        super(publish_date, archived)
+        @on_spotify = on_spotify
+    end
+
+    private
+
+    def can_be_archived?
+        @on_spotify && @archived
+    end
+end
+
+end
