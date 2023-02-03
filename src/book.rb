fddc1b5d7c19edb7c824225c473968fedb0e658e
nunegabriel <nune.gabriel.gn@gmail.com>
2023-02-03 18:39:20 +0000

    ADD BOOK

--- src/book.rb
+++ src/book.rb
@@ -1,8 +1,6 @@
 require_relative './item'
 
-# Create a Book class in a separate .rb file.
 class Book < Item
-  # All Book class properties visible in the diagram should be defined and set up in the constructor method.
   attr_accessor :name, :publisher, :cover_state, :label, :author, :publish_date
 
   def initialize(name, publisher, publish_date, cover_state)
@@ -13,34 +11,39 @@ class Book < Item
     @publish_date = publish_date
   end
 
-  # Implement methods:
-  #   can_be_archived?() in the Book class
-  #     should override the method from the parent class
-  #     should return true if parent's method returns true OR if cover_state equals to "bad"
-  #     otherwise, it should return false
-
+ 
   def can_be_archived?
     super || cover_state == 'bad'
   end
 
-  # All data should be preserved by saving collections in .json files.
-  def as_json()
+
+  def as_json
     {
       id: @id,
       publisher: @publisher,
       publish_date: @publish_date,
       cover_state: @cover_state,
       archived: @archived,
-      label: {
-        id: label.id,
-        title: label.title,
-        color: label.color
-      },
-      author: {
-        id: author.id,
-        first_name: author.first_name,
-        last_name: author.last_name
-      }
+      label: label_json,
+      author: author_json
     }
   end
-end
+  
+  private
+  
+  def label_json
+    {
+      id: label.id,
+      title: label.title,
+      color: label.color
+    }
+  end
+  
+  def author_json
+    {
+      id: author.id,
+      first_name: author.first_name,
+      last_name: author.last_name
+    }
+  end
+ end
